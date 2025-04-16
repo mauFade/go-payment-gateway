391b688cb4aa5a8a22789a8dd4c93f7d5ff0c64b
Mauricio Cardoso <mauricio.cds00@gmail.com>
2025-04-16 15:53:40 -0300

    feat: add handler for create acc

---
+++ internal/web/handlers/account-handler.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/mauFade/go-payment-gateway/internal/dto"
+	"github.com/mauFade/go-payment-gateway/internal/service"
+)
+
+type AccountHandler struct {
+	service service.AccountService
+}
+
+func NewAccountHandler(s service.AccountService) *AccountHandler {
+	return &AccountHandler{
+		service: s,
+	}
+}
+
+func (h *AccountHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var input dto.CreateAccountRequest
+	err := json.NewDecoder(r.Body).Decode(&input)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	res, err := h.service.CreateAccount(input)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
