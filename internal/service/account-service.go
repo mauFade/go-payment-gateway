c4d5dd4a3fc686e3bcbc61e19353700ec28cacd4
Mauricio Cardoso <mauricio.cds00@gmail.com>
2025-04-16 15:39:56 -0300

    feat: finish create account

--- internal/service/account-service.go
+++ internal/service/account-service.go
@@ -1,6 +1,9 @@
 package service
 
-import "github.com/mauFade/go-payment-gateway/internal/domain"
+import (
+	"github.com/mauFade/go-payment-gateway/internal/domain"
+	"github.com/mauFade/go-payment-gateway/internal/dto"
+)
 
 type AccountService struct {
 	repository domain.AccountRepository
@@ -12,4 +15,26 @@ func NewAccountService(r domain.AccountRepository) *AccountService {
 	}
 }
 
-func (s *AccountService) CreateAccount()
+func (s *AccountService) CreateAccount(input dto.CreateAccountRequest) (*dto.CreateAccountResponse, error) {
+	account := dto.ToAccount(input)
+
+	existingAccount, err := s.repository.FindByAPIKey(account.APIKey)
+
+	if err != nil && err != domain.ErrAccountNotFound {
+		return nil, err
+	}
+
+	if existingAccount != nil {
+		return nil, domain.ErrDuplicateAPIKey
+	}
+
+	err = s.repository.Save(account)
+
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+
+	return &output, nil
+}
