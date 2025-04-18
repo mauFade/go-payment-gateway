f5ec49c1bd50ac344f8323530f5428375c966c8a
Mauricio Cardoso <mauricio.cds00@gmail.com>
2025-04-18 20:38:49 -0300

    feat: add main connections

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func getEnv(key, defaultValue string) string {
+	return ""
+}
+
 func main() {
-	
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("error loading .env file.")
+	}
+
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		getEnv("DB_HOST", "db"),
+		getEnv("DB_PORT", "5432"),
+		getEnv("DB_USER", "postgres"),
+		getEnv("DB_PASSWORD", "postgres"),
+		getEnv("DB_NAME", "gateway"),
+		getEnv("DB_SSL_MODE", "disable"),
+	)
+
 }
