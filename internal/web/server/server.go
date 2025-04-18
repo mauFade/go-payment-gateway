8ecb9bdcff5f61b2e8b2ebda6f90f6ddec72b6e4
Mauricio Cardoso <mauricio.cds00@gmail.com>
2025-04-18 20:17:50 -0300

    feat: init router

---
+++ internal/web/server/server.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/mauFade/go-payment-gateway/internal/service"
+	"github.com/mauFade/go-payment-gateway/internal/web/handlers"
+)
+
+type Server struct {
+	router         *chi.Mux
+	server         *http.Server
+	accountService *service.AccountService
+	port           string
+}
+
+func NewServer(ac *service.AccountService, port string) *Server {
+	return &Server{
+		accountService: ac,
+		port:           port,
+		router:         chi.NewRouter(),
+	}
+}
+
+func (s *Server) ConfigureRoutes() {
+	accountHandler := handlers.NewAccountHandler(*s.accountService)
+
+	s.router.Post("/accounts", accountHandler.Create)
+	s.router.Get("/accounts", accountHandler.Get)
+}
