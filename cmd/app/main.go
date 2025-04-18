6ba70a545f45b2109ec04a9f7e0ddc60ef5b2cb4
Mauricio Cardoso <mauricio.cds00@gmail.com>
2025-04-18 20:39:05 -0300

    feat: add http

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/mauFade/go-payment-gateway/internal/repository"
+	"github.com/mauFade/go-payment-gateway/internal/service"
+	"github.com/mauFade/go-payment-gateway/internal/web/server"
 )
 
 func getEnv(key, defaultValue string) string {
-	return ""
+	if value := os.Getenv(key); value != "" {
+		return value
+	} else {
+		return defaultValue
+	}
 }
 
 func main() {
@@ -26,4 +35,24 @@ func main() {
 		getEnv("DB_SSL_MODE", "disable"),
 	)
 
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatal("error connecting to db.")
+	}
+
+	defer db.Close()
+
+	accRepository := repository.NewAccountRepository(db)
+
+	accService := service.NewAccountService(accRepository)
+
+	port := getEnv("HTTP_PORT", "8080")
+
+	srv := server.NewServer(accService, port)
+	srv.ConfigureRoutes()
+
+	if err := srv.Start(); err != nil {
+		log.Fatal("error starting http server.")
+	}
 }
