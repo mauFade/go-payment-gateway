c252d73fa99eba332dca4f8cc8bcec77201f6269
Mauricio Cardoso <mauricio.cds00@gmail.com>
2025-04-19 18:39:19 -0300

    feat: add invoice methods in repo

--- internal/repository/invoice-repository.go
+++ internal/repository/invoice-repository.go
@@ -75,3 +75,65 @@ func (r *InvoiceRepository) FindByID(id string) (*domain.Invoice, error) {
 
 	return &invoice, nil
 }
+
+func (r *InvoiceRepository) FindByAccountID(accId string) ([]*domain.Invoice, error) {
+	invoices := []*domain.Invoice{}
+
+	rows, err := r.db.Query(`
+    SELECT id, account_id, amount, status, description, payment_type, card_last_digits, created_at, updated_at
+    FROM invoices
+    WHERE account_id = $1
+`, accId)
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var invoice domain.Invoice
+		err := rows.Scan(
+			&invoice.ID,
+			&invoice.AccountID,
+			&invoice.Amount,
+			&invoice.Status,
+			&invoice.Description,
+			&invoice.PaymentType,
+			&invoice.LastCardDigits,
+			&invoice.CreatedAt,
+			&invoice.UpdatedAt,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		invoices = append(invoices, &invoice)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return invoices, nil
+}
+
+func (r *InvoiceRepository) UpdateStatus(i *domain.Invoice) error {
+	rows, err := r.db.Exec("UPDATE invoices SET status = $1, updated_at = $2 WHERE id = $3", i.Status, i.UpdatedAt, i.ID)
+
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := rows.RowsAffected()
+
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return domain.ErrInvoiceNotFound
+	}
+
+	return nil
+}
