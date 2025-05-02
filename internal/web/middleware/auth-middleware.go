051b01e3d43d654859a853cc81fbd49432c4bef6
Mauricio Cardoso <mauricio.cds00@gmail.com>
2025-05-02 18:57:41 -0300

    feat: add auth middleware

---
+++ internal/web/middleware/auth-middleware.go
@@ -0,0 +1,40 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/mauFade/go-payment-gateway/internal/domain"
+	"github.com/mauFade/go-payment-gateway/internal/service"
+)
+
+type AuthMiddleware struct {
+	accService service.AccountService
+}
+
+func NewAuthMiddleware(accService service.AccountService) *AuthMiddleware {
+	return &AuthMiddleware{
+		accService: accService,
+	}
+}
+
+func (m *AuthMiddleware) Authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		apiKey := r.Header.Get("X-API-KEY")
+		if apiKey == "" {
+			http.Error(w, "api key is required", http.StatusUnauthorized)
+			return
+		}
+
+		_, err := m.accService.FindByAPIKey(apiKey)
+		if err != nil {
+			if err == domain.ErrAccountNotFound {
+				http.Error(w, err.Error(), http.StatusUnauthorized)
+				return
+			}
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
